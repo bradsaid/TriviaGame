ddb56d26bb42729c3ce04311d3b83f890dcc9dd3
Brad Said <bradsaid@gmail.com>
2018-05-28 14:17:00 -0500

    Saving work

--- assets/javascript/index1.js
+++ assets/javascript/index1.js
@@ -1,61 +1,112 @@
 $( document ).ready(function(){
     let timer = $("#timer");
-    let question = $("#question");
-    let answer = $("#answer");
-    let btns = $("#answerButtons")
-    /*
-    for (var i = 0; i < qanda.questions.length; i++) {   
-      question.text(qanda.questions[i]);  // getting question
-      //let questionsToAnswer = $('<h1>' + qanda.questions[i] + '</h1>');
-      //question.append(questionsToAnswer);
-      let radioBtn = $('<label><input type="radio" name="group">' + qanda.answers[i] + '</label>');  // getting answers
-      radioBtn.addClass("answer-button");
-      radioBtn.attr("value", qanda.answers[i]);
-      btns.append(radioBtn);
-    }*/
- 
-    let questionsObject = [
-        {
-            prompt: "1st Question\n(a) 1\n\(b) 2\n\(c) 3",
-            answer : "a"
-        },
-        {
-            prompt: "2nd Question\n(a) 4\n\(b) 5\n\(c) 6",
-            answer : "b"
-        },]
-    let score = 0;
+    let q1 = "1?";
+    let q2 = "2?";
+    let q3 = "3?";
+    let q1a = "a";
+    let q2a = "b";
+    let q3a = "c";
+    let wrongAnswerArr = ["x", "y", "z", "f", "c"];
+    let wrongAnswerArr1 = [];
+    let radio1 = [];
+    let radio2 = [];
+    let right = 0;
+    let wrong = 0;
+    let randomRadio = "";
+    let submitButton = $('<button>Submit</button>');
+    $('#submit').append(submitButton);
 
+    let startAlert;
 
-    /*for (var i = 0; i < qanda[0].question.length; i++) {  
-        let questionsToAnswer = $('<h1>' + qanda[i].question + '</h1>');
-        question.append(questionsToAnswer);*/
-    
+    $("#start").on("click", function() {
+      startAlert = setTimeout(function() {
+      $('#submit').click(); 
+      }, 1000);
+    });
 
-    for (let i = 0; i < questionsObject.length; i++) {
-        //question.text(questionsObject[i].prompt);    // need to create question divs just like radio buttons
-        //function myFunction(item, index) {
-        //    let q = $('<h1>' + questionsObject.prompt + '</h1>');
-        //    return q;
-        //    console.log(q);
-        //}
-        //questionsObject.forEach(myFunction);
-        question.text(questionsObject[i].prompt);
-        console.log(questionsObject[i].prompt);
-        //question.append($(this)(q));
-        /*let response = window.prompt(questionsObject[i].prompt);
-        if (response == questionsObject[i].answer) {
-            score++;
-            alert("Correct!");
-        } else {  
-            alert("Wrong!");  
-        } */
+    function startGame() {
+
     }
 
-  
+    function q1Function() {
+        let randomOrder = wrongAnswerArr.sort(() => .5 - Math.random()).slice(0,4);  // shuffle array and pop 4 values into new array
+        radio1.push(q1a, randomOrder[0], randomOrder[1], randomOrder[2], randomOrder[3]); // add to array  
+        radio1.sort(function(a, b){return 0.5 - Math.random()}); // reshuffle array
+        let q11 = $('<h1>' + q1 + '</h1>');
+        $("#question1").append(q11);
+        for ( var i = 0; i < radio1.length; i++) {  // loop to assign array values to buttons
+            let radioBtn = $('<label><input type="radio" name="group" value="' + radio1[i] + '">' + radio1[i] + '</label>');
+            $("#answers1").append(radioBtn);
+        }
+    }
+    q1Function();
 
+    function q2Function() {
+        let randomOrder = wrongAnswerArr.sort(() => .5 - Math.random()).slice(0,4);  // shuffle array
+        radio1 = []; // clear array from question 1
+        radio1.push(q2a, randomOrder[0], randomOrder[1], randomOrder[2], randomOrder[3]); // add to array
+        radio1.sort(function(a, b){return 0.5 - Math.random()}); // reshuffle array
+        let q22 = $('<h1>' + q2 + '</h1>');
+        $("#question2").append(q22);
+        for ( var i = 0; i < radio1.length; i++) {  // loop to assign array values to buttons
+            let radioBtn = $('<label><input type="radio" name="group1" value="' + radio1[i] + '">'+ radio1[i] + '</label>');
+            $("#answers2").append(radioBtn);
+        }
+    }
+    q2Function();
 
-    //alert("you got " + score + "/" + questionsObject.length);
+    function q3Function() {
+        let randomOrder = wrongAnswerArr.sort(() => .5 - Math.random()).slice(0,4);  // shuffle array
+        radio1 = []; // clear array from question 1
+        radio1.push(q3a, randomOrder[0], randomOrder[1], randomOrder[2], randomOrder[3]); // add to array
+        radio1.sort(function(a, b){return 0.5 - Math.random()}); // reshuffle array
+        let q33 = $('<h1>' + q3 + '</h1>');
+        $("#question3").append(q33);
+        for ( var i = 0; i < radio1.length; i++) {  // loop to assign array values to buttons
+            let radioBtn = $('<label><input type="radio" name="group2" value="' + radio1[i] + '">' + radio1[i] + '</label>');
+            $("#answers3").append(radioBtn);
+        }
+    }
+    q3Function();
 
+    
 
-  }); 
+    $("#submit").click(function () {
+        $("input:radio[name='group']:checked").each(function() {  
+            let answerVal1 = $(this).attr("value");
+            console.log(answerVal1);
+            if (answerVal1 == q1a) {
+                right++;
+                $('#correctAnswers').text(right);
+            } else {
+                wrong++;
+                $('#wrongAnswers').text(wrong);
+            }
+        });
+        $("input:radio[name='group1']:checked").each(function() {
+            let answerVal2 = $(this).attr("value");
+            console.log(answerVal2);
+            if (answerVal2 == q2a) {
+                right++;
+                $('#correctAnswers').text(right);
+            } else {
+                wrong++;
+                $('#wrongAnswers').text(wrong);
+            }
+        });
+        $("input:radio[name='group2']:checked").each(function() {
+            let answerVal3 = $(this).attr("value");
+            console.log(answerVal3);
+            if (answerVal3 == q3a) {
+                right++;
+                $('#correctAnswers').text(right);
+            } else {
+                wrong++;
+                $('#wrongAnswers').text(wrong);
+            }
+        });
+
+    });
+    
+}); 
 
